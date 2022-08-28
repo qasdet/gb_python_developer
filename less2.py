6d522d9da00f942640d48809998da6dc0744fb84
alexandr86 <alexandr86@live.ru>
2022-08-28 23:32:43 +0300

    add 2 Урок

--- less2.py
+++ less2.py
@@ -4,8 +4,8 @@
 # 67,82 -> 23
 # 0,56 -> 11
 
-float_num = input('input float number: ')
-# print(type(float_num))
+float_num = input('Введите число с плавающей точкой: ')
+print(type(float_num))
 sum = 0
 for i in float_num:
     if i != '.':
@@ -15,13 +15,33 @@ print(sum)
 # 2. Напишите программу, которая принимает на вход число N
 # и выдает набор произведений чисел от 1 до N.
 # Пример:
-# пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 12, 123, 1234)
+# пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4) """
 
-n = int(input('input N: '))
-factorial = 1
-for i in range(1, n+1):
-    factorial *= i
-    print(factorial, end=' ')
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Число должно быть integer ")
+    return number
+
+
+def mult(n):
+    if n == 1:
+        return 1
+    else:
+        return n * mult(n - 1)
+
+
+num = InputNumbers("Введите число: ")
+
+list = []
+for e in range(1, num + 1):
+    list.append(mult(e))
+
+print(f"Произведения чисел от 1 до {num}:  {list}")
 
 
 # 3. Реализуйте случайное перемешивание списка.
